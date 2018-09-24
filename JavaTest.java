eb76479721b358b1782f6b6b2324b0c1e608921b
sajanauk <sajanauk@gmail.com>
2018-09-24 16:33:06 +0530

    Commited

---
+++ JavaTest.java
@@ -0,0 +1,12 @@
+
+package javatest;
+public class JavaTest {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+     System.out.println("Hellow test git");
+    }
+    
+}
